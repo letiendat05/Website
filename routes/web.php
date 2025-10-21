247e12aa6f384b09aea181b34a174841c031f22e
td.global <151906762+letiendat05@users.noreply.github.com>
2025-10-21 17:29:29 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +12,30 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/dangnhap', 'dangnhap');
+Route::view('/dangky', 'dangky');  
+
+Route::get('/phuongtien', function () {
+    return view('QLPT');
+});
+Route::get('/khachhang', function () {
+    return view('QLKH');
+});
+Route::get('/thanhtoan', function () {
+    return view('ThanhToan');
+});
+Route::get('/hopdong', function () {
+    return view('QLHD');
+});
+Route::get('/baocao', function () {
+    return view('BaoCao');
+});
+require __DIR__.'/auth.php';
